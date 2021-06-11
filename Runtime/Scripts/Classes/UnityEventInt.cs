d01be325361a746153dfb7da902fa71144dc499a
Kyle Lamothe <alscenic@gmail.com>
2021-06-11 10:44:45 -0400

    Added UnityEventInt

---
+++ Runtime/Scripts/Classes/UnityEventInt.cs
@@ -0,0 +1,13 @@
+﻿// Code by Kyle Lamothe
+// from current.gen Studios
+
+using UnityEngine.Events;
+
+namespace CGenStudios.UnityUtils.UnityEvents
+{
+	/// <summary>
+	/// The UnityEventInt.
+	/// </summary>
+	[System.Serializable]
+	public sealed class UnityEventInt : UnityEvent<int> { }
+}
